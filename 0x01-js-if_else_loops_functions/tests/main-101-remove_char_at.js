4400e20e67c0ca76fdad91b2550265f43d1435cb
Carlos Soria <soriac367@gmail.com>
2022-06-02 11:59:53 -0500

    Tests for 101-remove_char_at

---
+++ 0x01-js-if_else_loops_functions/tests/main-101-remove_char_at.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const rm = require("../101-remove_char_at");
+
+console.log(rm.removeCharAt("Best School", 3))
+console.log(rm.removeCharAt("Chicago", 2))
+console.log(rm.removeCharAt("C is fun!", 0))
+console.log(rm.removeCharAt("School", 10))
+console.log(rm.removeCharAt("Python", -2))
