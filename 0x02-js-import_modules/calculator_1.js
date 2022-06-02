423b050b9d907b244dc497c367f1f5c0252b4f0e
Carlos Soria <soriac367@gmail.com>
2022-06-02 14:46:10 -0500

    Source for task 1-calculation.js

---
+++ 0x02-js-import_modules/calculator_1.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+function add(x, y) {
+    return x + y
+}
+
+function sub(x, y) {
+    return x - y
+}
+
+function div(x, y) {
+    return x / y
+}
+
+function mul(x, y) {
+    return x * y
+}
+export {add, sub, div, mul}
