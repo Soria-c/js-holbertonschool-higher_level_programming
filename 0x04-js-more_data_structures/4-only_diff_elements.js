f96f0c521712a24bfc8dfdb144dfa4a5e0c34250
Carlos Soria <soriac367@gmail.com>
2022-06-03 16:10:26 -0500

    Symmetric difference

---
+++ 0x04-js-more_data_structures/4-only_diff_elements.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function onlyDiffElements(set1, set2) {
+  let dif1 = Array.from(set1).filter((x) => !set2.has(x));
+  let dif2 = Array.from(set2).filter((x) => !set1.has(x))
+  return new Set(dif1.concat(dif2))
+} 
+
+
+const a = new Set(["Python", "C", "Javascript"]);
+const b = new Set(["Bash", "C", "Ruby", "Perl"]);
+console.log(onlyDiffElements(a, b))
